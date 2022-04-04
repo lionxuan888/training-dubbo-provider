8597805b33376af30b5b3ee6082f59ca89ae6055
jinxiaoxuan <jinxiaoxuan@xiaomi.com>
2022-04-04 15:35:06 +0800

    111

--- dubbo-provider/src/main/java/com/tian/controller/ApiController.java
+++ dubbo-provider/src/main/java/com/tian/controller/ApiController.java
@@ -1,9 +1,12 @@
 package com.tian.controller;
 
+import com.tian.service.TestService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
 
 /**
  * Created by logan on 2020/3/11.
@@ -12,9 +15,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/api")
 public class ApiController {
 
+    @Resource
+    private TestService testService;
+
     @RequestMapping(value = "/test", method = RequestMethod.GET)
     @ResponseBody
     public String test() {
+        testService.test();
       return "hello server";
     }
 
---
+++ dubbo-provider/src/main/java/com/tian/service/TestService.java
@@ -0,0 +1,11 @@
+package com.tian.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+
+    public void test () {
+        System.out.println("test Service");
+    }
+}
